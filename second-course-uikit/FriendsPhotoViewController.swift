61c1d7a4d3a18005d05efb7fe895c983fb361ac3
MuryginaNatalya <nataly-mna@yandex.ru>
2022-03-22 15:21:08 +0400

    HW two is fineshed

---
+++ second-course-uikit/FriendsPhotoViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FriendsPhotoViewController.swift
+//  second-course-uikit
+//
+//  Created by Natalya Murygina on 22.03.2022.
+//
+
+import UIKit
+
+class FriendsPhotoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
